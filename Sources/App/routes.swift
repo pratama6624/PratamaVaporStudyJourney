dcebce4200188c1816613a68fe7bf77558865169
pratama6624 <scyoung6624@gmail.com>
2025-01-24 00:06:51 +0700

    Vapor basics routing - Dynamic route parameters (done)

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,12 +2,35 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    // /
     app.get { req async in
         "It works!"
     }
 
+    // /hello
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    // /hello/vapor
+    app.get("hello", "vapor") { req async -> String in
+        "Hello, Vapor!"
+    }
+    
+    // /hey
+    app.on(.GET, "hey") { req async -> String in
+        "Hey!"
+    }
+    
+    // /hey/vapor
+    app.on(.GET, "hey", "vapor") { req async -> String in
+        "Hey, Vapor!"
+    }
+    
+    // Dynamic route
+    app.on(.GET, "hello", ":name") { req async -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
     }
 
     try app.register(collection: TodoController())
