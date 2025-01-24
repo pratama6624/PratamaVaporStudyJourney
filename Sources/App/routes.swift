7b6b2353f82b1b7a97381c67ac957ee529e887aa
pratama6624 <scyoung6624@gmail.com>
2025-01-24 23:06:51 +0700

    Vapor basics routing - OPTIONS (done)

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,36 +2,43 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    // /
+    // -> GET /
     app.get { req async in
         "It works!"
     }
 
-    // /hello
+    // -> GET /hello
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
     
-    // /hello/vapor
+    // -> GET /hello/vapor
     app.get("hello", "vapor") { req async -> String in
         "Hello, Vapor!"
     }
     
-    // /hey
+    // -> GET /hey
     app.on(.GET, "hey") { req async -> String in
         "Hey!"
     }
     
-    // /hey/vapor
+    // -> GET /hey/vapor
     app.on(.GET, "hey", "vapor") { req async -> String in
         "Hey, Vapor!"
     }
     
-    // Dynamic route
+    // Dynamic route -> GET /hello/:name
     app.on(.GET, "hello", ":name") { req async -> String in
         let name = req.parameters.get("name")!
         return "Hello, \(name)!"
     }
 
+    // -> OPTIONS /api/users
+    app.on(.OPTIONS, "api", "users") { req in
+        return Response(status: .ok, headers: [
+            "Allow" : "GET, POST, PUT, DELETE"
+        ])
+    }
+    
     try app.register(collection: TodoController())
 }
