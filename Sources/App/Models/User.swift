bb31facb16d77afc7b4802a4377a8309acc62900
pratama6624 <scyoung6624@gmail.com>
2025-01-31 22:45:37 +0700

    Vapor basics content - Hooks afterDecode JSON to Object Model (done)

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -10,6 +10,19 @@ import Vapor
 // Smaple -> How Content Works
 struct User: Content {
     var id: UUID?
-    var name: String
+    var name: String?
     var age: Int
+    
+    // afterDevode (Setelah request diterima dan didecode / JSON to Object Model)
+    mutating func afterDecode() throws {
+        // Hilangkan space di awala dan akhir string (jika ada)
+        self.name = self.name?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Jika nama ada tetapi kosong setelah trim maka lempar ke badRequest
+        if let name = self.name, name.isEmpty {
+            throw Abort(.badRequest, reason: "Name must not be empty")
+        }
+    }
+    
+    
 }
