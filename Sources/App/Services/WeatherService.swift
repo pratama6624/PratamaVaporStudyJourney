bbb9adf1f45c73b84ca52631d6ec74e2bad35786
pratama6624 <scyoung6624@gmail.com>
2025-02-10 23:10:21 +0700

    Vapor basics client - Add Service API OpenWeather (done)

--- Sources/App/Services/WeatherService.swift
+++ Sources/App/Services/WeatherService.swift
@@ -10,5 +10,18 @@ import Vapor
 struct WeatherService {
     let client: Client
     
-    
+    func getWeather(for city: String) async throws -> WeatherResponse {
+        let apiKey = "3f791f28c2dbc76d12631dcfe118e1e5"
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
+        
+        let response = try await client.get(URI(string: url))
+        
+        guard let body = response.body else {
+            throw Abort(.badRequest, reason: "Invalid response from OpenWeather API")
+        }
+        
+        let decodedResponse = try JSONDecoder().decode(WeatherResponse.self, from: body)
+        
+        return decodedResponse
+    }
 }
