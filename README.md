2ff2eb4aca3083f67bd895c7accae18a0c42b1d9
pratama6624 <scyoung6624@gmail.com>
2025-02-16 20:28:03 +0700

    Vapor basics validation - Specific Validation (done)

--- README.md
+++ README.md
@@ -70,5 +70,6 @@ This repository is created to document my journey in learning **Vapor**, a Swift
     [√] Validation HRE ( Human-Readable Errors )
     [√] Validation HRE Format Response ( AbortError Vapor Default )
     [√] Validation HRE Format Response ( Custom Format Response Middleware )
-    [ ]
+    [√] Validation ( Specific Validation )
+    [ ] Validation ( Validatable Protocol & Validator List )
 ```
--- Sources/App/DTOs/ValidationDTO.swift
+++ Sources/App/DTOs/ValidationDTO.swift
@@ -8,12 +8,15 @@
 import Vapor
 
 struct ValidationDTO: Content, Validatable {
-    var name: String
-    var age: Int
-    var favoriteColor: Color
+    let name: String
+    let email: String
+    let age: Int
+    let favoriteColor: Color
     
     static func validations(_ validations: inout Vapor.Validations) {
         validations.add("name", as: String.self, is: .count(3...), required: true)
+        // Specific Validation
+        validations.add("email", as: String.self, is: .email, required: true)
         validations.add("age", as: Int.self, is: .range(18...), required: true)
         validations.add("favoriteColor", as: String.self, is: .in("red", "blue", "green"), required: true)
     }
